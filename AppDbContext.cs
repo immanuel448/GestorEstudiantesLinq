af0e7e263f72080c872d442e3ab6ff61cb25078b
Emmanuel Mendoza <emmanuelmendoza448@gmail.com>
2025-07-27 20:22:53 -0600

    se sseparan las clases para mejorar la legibilidad

---
+++ AppDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestorEstudiantesLinq
+{
+    // Contexto de Entity Framework Core para manejar la conexión y las operaciones con SQLite
+    public class AppDbContext : DbContext
+    {
+        /*
+         * Paquetes NuGet requeridos para funcionar:
+         * - Microsoft.EntityFrameworkCore
+         * - Microsoft.EntityFrameworkCore.Sqlite
+         * - Microsoft.EntityFrameworkCore.Tools
+         */
+
+        // Representa la tabla 'Estudiantes' en la base de datos
+        public DbSet<Estudiante> Estudiantes { get; set; }
+
+        // Configura la conexión con la base de datos SQLite
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // Si el archivo 'estudiantes.db' no existe, se crea automáticamente al ejecutarse
+            optionsBuilder.UseSqlite("Data Source=estudiantes.db");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,32 +6,6 @@ using System.Linq;
 
 namespace GestorEstudiantesLinq
 {
-    //**esto es importante para instalar paquetes
-    //dotnet nuget list source
-    //dotnet nuget remove source packagesource1q
-
-
-
-    // Contexto de Entity Framework Core para manejar la conexión y las operaciones con SQLite
-    public class AppDbContext : DbContext
-    {
-        /*
-         * Paquetes NuGet requeridos para funcionar:
-         * - Microsoft.EntityFrameworkCore
-         * - Microsoft.EntityFrameworkCore.Sqlite
-         * - Microsoft.EntityFrameworkCore.Tools
-         */
-
-        // Representa la tabla 'Estudiantes' en la base de datos
-        public DbSet<Estudiante> Estudiantes { get; set; }
-
-        // Configura la conexión con la base de datos SQLite
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            // Si el archivo 'estudiantes.db' no existe, se crea automáticamente al ejecutarse
-            optionsBuilder.UseSqlite("Data Source=estudiantes.db");
-        }
-    }
 
     class Program
     {
