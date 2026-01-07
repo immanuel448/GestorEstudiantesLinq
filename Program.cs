2e838c672ab481638ad65f60ed6108cc6944ff62
Immanuel <emmanuelmendoza448@gmail.com>
2026-01-06 20:26:47 -0600

    fix: carga inicial de estudiantes desde la base de datos

--- Program.cs
+++ Program.cs
@@ -25,6 +25,7 @@ namespace GestorEstudiantesLinq
 
             // ya se toman los datos desde la BD
             using var db = new AppDbContext();
+            db.Database.EnsureCreated(); // crea bd y tablas si no existen
             //y se convierte a una lista
             List<Estudiante> estudiantes = db.Estudiantes.ToList();
 
